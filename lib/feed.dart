66c58a7317fb9af6f8ecd4031a5d7c8cee48b53d
Mkohler4 <mkohler4@uwo.ca>
2021-11-08 03:11:25 -0500

    icon position

--- lib/feed.dart
+++ lib/feed.dart
@@ -3,3 +3,4 @@ library Feed;
 export 'package:feed/feeds/simple_multi_feed.dart';
 export 'package:feed/feeds/swipe_feed.dart';
 export 'package:feed/feeds/compact_feed.dart';
+export 'package:feed/util/icon_position.dart';
---
+++ lib/util/icon_position.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+///The positionings for the icon
+enum IconPosition {
+  LEFT, RIGHT, TOP, BOTTOM
+}
+
+extension IconPostionExtension on IconPosition{
+
+  DismissDirection? get direction{
+    switch (this) {
+      case IconPosition.LEFT:
+        return DismissDirection.endToStart;
+      case IconPosition.RIGHT:
+        return DismissDirection.startToEnd;
+      case IconPosition.TOP:
+        return DismissDirection.down;
+      case IconPosition.BOTTOM:
+        return DismissDirection.up;
+      default:
+        return null;
+    }
+  }
+
+  IconPosition? fromDirection(DismissDirection direction){
+    switch (direction) {
+      case DismissDirection.endToStart:
+        return IconPosition.LEFT;
+      case DismissDirection.startToEnd:
+        return IconPosition.RIGHT;
+      case DismissDirection.down:
+        return IconPosition.BOTTOM;
+      case DismissDirection.up:
+        return IconPosition.TOP;
+      default:
+        return null;
+    }
+  }
+}
