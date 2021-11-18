ef069b9968afa17b0ced2a2ae84c2accb39e9bc6
Mkohler4 <mkohler4@uwo.ca>
2021-11-18 05:28:14 -0500

    single feed

--- lib/feed.dart
+++ lib/feed.dart
@@ -4,4 +4,3 @@ export 'package:feed/feeds/multi_feed.dart';
 export 'package:feed/feeds/feed.dart';
 export 'package:feed/feeds/swipe_feed.dart';
 export 'package:feed/feeds/compact_feed.dart';
-export 'package:feed/feeds/sliding_sheet_feed.dart';
