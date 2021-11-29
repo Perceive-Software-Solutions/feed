a8caccebea5afc22cc3e60dbb175ae233a6e5491
Mkohler4 <mkohler4@uwo.ca>
2021-11-29 05:51:09 -0500

    updated changelog

--- lib/feed.dart
+++ lib/feed.dart
@@ -5,3 +5,5 @@ export 'package:feed/feeds/feed.dart';
 export 'package:feed/feeds/swipe_feed.dart';
 export 'package:feed/feeds/compact_feed.dart';
 export 'package:feed/feeds/sliding_sheet_feed.dart';
+export 'package:feed/util/icon_position.dart';
+export 'package:feed/feeds/feed_list_view.dart';
