937748ec9f89e8247fd3e519f6f592443048c442
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2021-12-30 01:13:05 -0500

    updated export

--- lib/feed.dart
+++ lib/feed.dart
@@ -3,6 +3,7 @@ library Feed;
 export 'package:feed/feeds/multi_feed.dart';
 export 'package:feed/feeds/feed.dart';
 export 'package:feed/feeds/swipe_feed.dart';
+export 'package:feed/animated/swipe_feed_card.dart';
 export 'package:feed/feeds/compact_feed.dart';
 export 'package:feed/feeds/sliding_sheet_feed.dart';
 export 'package:feed/util/icon_position.dart';
