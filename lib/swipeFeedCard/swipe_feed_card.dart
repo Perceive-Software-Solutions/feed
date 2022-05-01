1f84afe1d34f0151bc30126a0f67697efbafcd00
Mkohler4 <mkohler4@uwo.ca>
2022-05-01 01:49:36 -0400

    updated version

--- lib/swipeFeedCard/swipe_feed_card.dart
+++ lib/swipeFeedCard/swipe_feed_card.dart
@@ -250,8 +250,7 @@ class _SwipeFeedCardState<T> extends State<SwipeFeedCard> {
                         padding: !show ? const EdgeInsets.only(top: 74, bottom: 12, left: 8, right: 8) : EdgeInsets.zero,
                         child: SwipeCard(
                           controller: swipeCardController,  
-                          swipable: true,
-                          // swipable: !keyboard && (state is SwipeCardShowState && (widget as SwipeFeedCard<T>).item.item1 != null) || (state is SwipeCardExpandState && !keyboard),
+                          swipable: !keyboard && (state is SwipeCardShowState && (widget as SwipeFeedCard<T>).item.item1 != null) || (state is SwipeCardExpandState && !keyboard),
                           onPanUpdate: _onPanUpdate,
                           onSwipe: _onSwipe,
                           child: Container(
